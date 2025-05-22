a092a6c2cf46d2f536510c4f44a71f24a496e8ae
Guilherme Sousa <guilherme.sousa@fpf.org.br>
2025-05-21 21:37:44 -0300

    feat: Ajuste no swagger

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,6 +3,7 @@ using Domain.Interfaces.Repositories;
 using Domain.Interfaces.ServicosExternos;
 using Infrastructure.Repositories;
 using Infrastructure.ServicosExternos;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Serviço de Produção de Pedidos", Version = "v1" });
+});
 
 // Injeção de dependências para Repositórios
 builder.Services.AddScoped<IProducaoContext, ProducaoContext>();
